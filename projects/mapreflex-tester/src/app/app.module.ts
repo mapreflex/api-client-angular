19a7adcb5265f876b21d93bd6186d32cd2f45f5d
Caiter <andry@seek.pe>
2019-05-09 09:32:13 +0300

    API_KEY

--- projects/mapreflex-tester/src/app/app.module.ts
+++ projects/mapreflex-tester/src/app/app.module.ts
@@ -5,8 +5,7 @@ import {AppComponent} from './app.component';
 import {MapreflexModule, Options} from '@mapreflex/api-client-angular';
 
 const options: Options = {
-  apiKey: '06495e8e-ef15-4d3d-a2ce-5691706eded0',
-  apiUrl: 'http://localhost:8033'
+  apiKey: 'API_KEY'
 };
 
 @NgModule({
